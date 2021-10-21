0f40be42d927b54797f362a6762e17d848e6b134
zxcv12456 <78541558+zxcv12456@users.noreply.github.com>
2021-10-21 21:45:46 +0800

    Add files via upload

--- MyRPC/src/main/java/com/ganghuan/myRPCVersion3/server/TestServer.java
+++ MyRPC/src/main/java/com/ganghuan/myRPCVersion3/server/TestServer.java
@@ -11,9 +11,6 @@ public class TestServer {
         UserService userService = new UserServiceImpl();
         BlogService blogService = new BlogServiceImpl();
 
-//        Map<String, Object> serviceProvide = new HashMap<>();
-//        serviceProvide.put("com.ganghuan.myRPCVersion2.service.UserService",userService);
-//        serviceProvide.put("com.ganghuan.myRPCVersion2.service.BlogService",blogService);
         ServiceProvider serviceProvider = new ServiceProvider();
         serviceProvider.provideServiceInterface(userService);
         serviceProvider.provideServiceInterface(blogService);
--- MyRPC/src/main/java/com/ganghuan/myRPCVersion3/service/UserServiceImpl.java
+++ MyRPC/src/main/java/com/ganghuan/myRPCVersion3/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserByUserId(Integer id) {
         // 模拟从数据库中取用户的行为
-        User user = User.builder().id(id).userName("he2121").sex(true).build();
+        User user = User.builder().id(id).userName("zxcv").sex(true).build();
         System.out.println("客户端查询了"+id+"用户");
         return user;
     }
