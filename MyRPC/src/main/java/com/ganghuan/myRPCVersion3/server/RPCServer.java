7aa80c58ec961e58158bcae0f68b9784dd3771a4
zxcv12456 <78541558+zxcv12456@users.noreply.github.com>
2022-03-22 02:05:27 +0800

    Delete MyRPC/src/main/java/com/ganghuan/myRPCVersion3 directory

--- MyRPC/src/main/java/com/ganghuan/myRPCVersion3/server/RPCServer.java
+++
@@ -1,6 +0,0 @@
-package com.ganghuan.myRPCVersion3.server;
-
-public interface RPCServer {
-    void start(int port);
-    void stop();
-}
--- MyRPC/src/main/java/com/ganghuan/myRPCVersion3/service/BlogService.java
+++
@@ -1,8 +0,0 @@
-package com.ganghuan.myRPCVersion3.service;
-
-
-import com.ganghuan.myRPCVersion3.common.Blog;
-
-public interface BlogService {
-    Blog getBlogById(Integer id);
-}
